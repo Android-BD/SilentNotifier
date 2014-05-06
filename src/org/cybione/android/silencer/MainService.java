eeec1cf984703d11ec6a4b8b814256821bca9618
Colin Didier <didier@archos.com>
2014-05-06 15:55:40 +0200

    style fixes

--- src/org/cybione/android/silencer/MainService.java
+++ src/org/cybione/android/silencer/MainService.java
@@ -4,6 +4,7 @@ import org.cybione.android.silencer.features.ForceSilencer;
 import org.cybione.android.silencer.features.VibrationNotifier;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -12,29 +13,33 @@ import android.os.PowerManager.WakeLock;
 
 @SuppressLint("Wakelock")
 public class MainService {
-	public static final String TAG = "MainSerivec";
+	public static final String TAG = "MainService";
 
 	private static MainService mInstance = null;
-	private static Object mLock = new Object();
-	private static WakeLock mWakeLock;
 
+	private Context mContext;
+	private Object mLock = new Object();
+	private WakeLock mWakeLock;
+	private BroadcastReceiver mIntentReceiver;
 	private VibrationNotifier mVibrationNotifier;
 
 	public MainService(Context context) {
 		mInstance = this;
+		mContext = context;
 
 		PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 		mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-				context.getResources().getText(R.string.app_name).toString());
+				mContext.getResources().getText(R.string.app_name).toString());
 	
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(Intent.ACTION_SCREEN_ON);
 		filter.addAction(IntentReceiver.INTENT_ACTION_PHONE_STATE);
 		filter.addAction(IntentReceiver.INTENT_TELEPHONY_SMS_RECEIVED);
-        context.registerReceiver(new IntentReceiver(), filter);
+		mIntentReceiver = new IntentReceiver();
+        mContext.registerReceiver(mIntentReceiver, filter);
 
 		mVibrationNotifier = new VibrationNotifier(context);
-		new ForceSilencer(context);
+		new ForceSilencer(mContext);
 	}
 
 	public static MainService getInstance() {
@@ -74,10 +79,7 @@ public class MainService {
 	}
 
 	public void onDestroy() {
-		synchronized (mLock) {
-			mWakeLock.acquire();
-			mInstance = null;
-			mWakeLock.release();
-		}
+		mInstance = null;
+		mContext.unregisterReceiver(mIntentReceiver);
 	}
 }
